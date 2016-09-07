7e1239e3450e715c13df9abc489e46ed14947c00
Nikhil srivastava <rishu.nss@gmail.com>
2016-09-07 12:00:48 +0530

    adding changes for enhancing the features

--- angular-query-builder.js
+++ angular-query-builder.js
@@ -16,9 +16,35 @@ app.controller('QueryBuilderCtrl', ['$scope', function ($scope) {
         if (!group) return "";
         for (var str = "(", i = 0; i < group.rules.length; i++) {
             i > 0 && (str += " <strong>" + group.operator + "</strong> ");
-            str += group.rules[i].group ?
+            
+            switch(group.rules[i].condition){
+                case "contains":
+                    str += group.rules[i].group ?
+                           computed(group.rules[i].group) :
+                           "("+group.rules[i].field +".includes("+ group.rules[i].data+"))";
+                    break;
+                    case "Does_Not_Contain":
+                    str += group.rules[i].group ?
+                           computed(group.rules[i].group) :
+                           "!("+group.rules[i].field +".includes("+ group.rules[i].data+"))";
+                    break;
+                    case "Starts_with":
+                    str += group.rules[i].group ?
+                           computed(group.rules[i].group) :
+                           "("+group.rules[i].field +".startsWith("+ group.rules[i].data+"))";
+                    break;
+                    case "Ends_with":
+                    str += group.rules[i].group ?
+                           computed(group.rules[i].group) :
+                           "("+group.rules[i].field +".endsWith("+ group.rules[i].data+"))";
+                    break;
+                    
+                default: 
+                    str += group.rules[i].group ?
                 computed(group.rules[i].group) :
-                group.rules[i].field + " " + htmlEntities(group.rules[i].condition) + " " + group.rules[i].data;
+                "( "+group.rules[i].field + htmlEntities(group.rules[i].condition) + group.rules[i].data+" )";
+                    break;
+            } 
         }
 
         return str + ")";
@@ -29,6 +55,7 @@ app.controller('QueryBuilderCtrl', ['$scope', function ($scope) {
     $scope.filter = JSON.parse(data);
 
     $scope.$watch('filter', function (newValue) {
+        console.log(newValue);
         $scope.json = JSON.stringify(newValue, null, 2);
         $scope.output = computed(newValue.group);
     }, true);
@@ -51,6 +78,31 @@ queryBuilder.directive("queryBuilderParent",function(){
           scope.handler.check = function(){
               console.log("handler check has been called");
           }
+          
+         // scope.handler.defaultColorCodedView = function(){
+              scope.group.rules=[{
+                        condition: '=',
+                        field: 'Firstname',
+                        data: ''
+                    },{
+                        condition: '=',
+                        field: 'Lastname',
+                        data: ''
+                    }
+                  ,{
+                        group: {"operator": "AND","rules": [ {
+                        condition: '=',
+                        field: 'Firstname',
+                        data: ''
+                        }, {
+                        condition: '=',
+                        field: 'Lastname',
+                        data: ''}
+                      ]}}
+                   ];
+                   
+         // }
+                
       }
  }
 })
@@ -67,6 +119,14 @@ queryBuilder.directive('queryBuilder', ['$compile', function ($compile) {
             var content, directive;
             content = element.contents().remove();
             return function (scope, element, attrs) {
+                
+               console.log(scope , content);
+                
+                if(scope.handler.defaultColorCodedView){
+                    
+                  scope.handler.defaultColorCodedView();
+                }
+                
                 scope.operators = [
                     { name: 'AND' },
                     { name: 'OR' }
@@ -76,26 +136,95 @@ queryBuilder.directive('queryBuilder', ['$compile', function ($compile) {
                     { name: 'Firstname' },
                     { name: 'Lastname' },
                     { name: 'Birthdate' },
-                    { name: 'City' },
-                    { name: 'Country' }
+                    { name: 'string' },
+                    { name: 'date' },
+                    { name: 'datetime' },
+                    { name: 'ss' }
                 ];
+                
 
-                scope.conditions = [
+
+                scope.conditions = 
+                {
+                  datetime : [ { name:"is equal to" , value:"===" },
+                               { name:"is not equal to" , value:"!==" },
+                               { name:"is after or equal to" , value:">=" },
+                               { name:"is after" , value:">" },
+                               { name:"is before or equal to" , value:"<=" },
+                               { name:"is before" , value:"<" },
+                               { name:"is null" , value:null },
+                               { name:"is not null" , value:!null }
+
+                             ],
+
+                  date : [ { name:"is equal to" , value:"===" },
+                               { name:"is not equal to" , value:"!==" },
+                               { name:"is after or equal to" , value:">=" },
+                               { name:"is after" , value:">" },
+                               { name:"is before or equal to" , value:"<=" },
+                               { name:"is before" , value:"<" },
+                               { name:"is null" , value:null },
+                               { name:"is not null" , value:!null }
+
+                             ],
+
+                  string : [   { name:"is equal to" , value:"===" },
+                               { name:"is not equal to" , value:"!==" },
+                               { name:"contains" , value:"contains" },
+                               { name:"Does not Contains" , value:"Does_Not_Contain" },
+                               { name:"Starts with" , value:"Starts_with" },
+                               { name:"Ends with" , value:"Ends_with)" },
+                               { name:"Less than or equal to" , value:"<=" },
+                               { name:"Less than" , value:"<" },
+                               { name:"Greater than or equal to" , value:">=" },
+                               { name:"Greater than" , value:">" }
+
+                             ],
+                    
+                    bool : [   { name:"is equal to" , value:"===" },
+                               { name:"is not equal to" , value:"!==" },
+                               { name:"contains" , value:"contains" },
+                               { name:"Does not Contains" , value:"Does_Not_Contain" },
+                               { name:"Starts with" , value:"Starts_with" },
+                               { name:"Ends with" , value:"Ends_with)" },
+                               { name:"Less than or equal to" , value:"<=" },
+                               { name:"Less than" , value:"<" },
+                               { name:"Greater than or equal to" , value:">=" },
+                               { name:"Greater than" , value:">" }
+
+                             ]
+
+                }
+                /*scope.conditions = [
                     { name: '=' },
                     { name: '<>' },
                     { name: '<' },
                     { name: '<=' },
                     { name: '>' },
                     { name: '>=' }
-                ];
+                ];*/
 
                 scope.addCondition = function () {
                     
                     scope.group.rules.push({
-                        //condition: '=',
-                        //  field: 'Firstname',
-                        //data: ''
-                    });
+                        condition: '=',
+                        field: 'Firstname',
+                        data: ''
+                    }, {
+                        condition: '=',
+                        field: 'Lastname',
+                        data: ''
+                    },{
+                        group: {"operator": "AND","rules": [ {
+                        condition: '=',
+                        field: 'Firstname',
+                        data: ''
+                        }, {
+                        condition: '=',
+                        field: 'Lastname',
+                        data: ''
+                     } ]}}
+                    );
                 };
 
                 
